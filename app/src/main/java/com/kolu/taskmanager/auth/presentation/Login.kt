38251ada8bffe6b17c156b5b31175395bf539390
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-01-11 14:54:32 +0600

    fix width of text field

--- app/src/main/java/com/kolu/taskmanager/auth/presentation/Login.kt
+++ app/src/main/java/com/kolu/taskmanager/auth/presentation/Login.kt
@@ -1,29 +1,32 @@
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.ui.Modifier
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
@@ -44,16 +47,19 @@ fun Login(
     BackgroundAuth(modifier = modifier)
     Column(
         modifier = Modifier
-            .padding(top = 186.dp, start = 64.dp)
+            .padding(top = 186.dp, start = 64.dp, end = 64.dp)
     ) {
         Text(
             text = "Get Started With",
             style = MaterialTheme.typography.headlineLarge,
             fontWeight = FontWeight.SemiBold
         )
+
         Spacer(Modifier.height(20.dp))
 
         OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth(),
             leadingIcon = {Icon(
                 imageVector = Icons.Default.Person,
                 contentDescription = null)},
@@ -64,6 +70,7 @@ fun Login(
             },
         )
         OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
             label = { Text(text = "password") },
             value = password,
             onValueChange = {
