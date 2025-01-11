9ed89e6ae9338d3048db868f18b61403e70331dd
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-01-11 17:34:32 +0600

    add forget pass and signup option

--- app/src/main/java/com/kolu/taskmanager/auth/presentation/Login.kt
+++ app/src/main/java/com/kolu/taskmanager/auth/presentation/Login.kt
@@ -1,14 +1,19 @@
+import android.widget.Toast
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.FilledIconButton
@@ -26,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -40,82 +46,131 @@ fun Login(
     modifier: Modifier = Modifier
         .fillMaxSize()
 ) {
+    val context = LocalContext.current
     var email by remember { mutableStateOf(TextFieldValue("")) }
     var password by remember { mutableStateOf(TextFieldValue("")) }
     var passwordVisibility by remember { mutableStateOf(false) }
     val passwordVisibilityIcon = if (passwordVisibility) painterResource(R.drawable.visible) else painterResource(R.drawable.invisible)
+
     BackgroundAuth(modifier = modifier)
-    Column(
+
+    Box(
         modifier = Modifier
-            .padding(top = 186.dp, start = 64.dp, end = 64.dp)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "Get Started With",
-            style = MaterialTheme.typography.headlineLarge,
-            fontWeight = FontWeight.SemiBold
-        )
-
-        Spacer(Modifier.height(20.dp))
-
-        OutlinedTextField(
-            singleLine = true,
+        Column(
             modifier = Modifier
-                .fillMaxWidth(),
-            leadingIcon = {Icon(
-                imageVector = Icons.Default.Person,
-                contentDescription = null)},
-            label = { Text(text = "email") },
-            value = email,
-            onValueChange = {
-                email = it
-            }
-        )
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            label = { Text(text = "password") },
-            value = password,
-            onValueChange = {
-                password = it
-            },
-            leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = null) },
-            trailingIcon = {
-                IconButton(onClick = {
-                    passwordVisibility = !passwordVisibility
-                }) {
-                    Icon(
-                        painter = passwordVisibilityIcon,
-                        contentDescription = null)
-                }
-            },
-            visualTransformation = if(passwordVisibility)
-                VisualTransformation.None else PasswordVisualTransformation(),
-            singleLine = true
-        )
-
-        Spacer(Modifier.height(20.dp))
-
-        FilledIconButton(
-            onClick = {
-                // todo
-            },
-            shape = RoundedCornerShape(10),
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .fillMaxWidth()
-        ) {
-            Icon(
-                imageVector = Icons.Default.ArrowForward,
-                contentDescription = "login button",
-                modifier = Modifier
-                    .clip(CircleShape)
-                    .border(
-                        width = 1.dp,
-                        color = LocalContentColor.current,
-                        shape = CircleShape
-                    )
+                .wrapContentSize()
+                .padding(start = 64.dp, end = 64.dp)
+        ){
+            Text(
+                text = "Get Started With",
+                style = MaterialTheme.typography.headlineLarge,
+                fontWeight = FontWeight.SemiBold
             )
-         }
+
+            Spacer(Modifier.height(20.dp))
+
+            OutlinedTextField(
+                singleLine = true,
+                modifier = Modifier
+                    .fillMaxWidth(),
+                leadingIcon = {Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = null)},
+                label = { Text(text = "email") },
+                value = email,
+                onValueChange = {
+                    email = it
+                }
+            )
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                label = { Text(text = "password") },
+                value = password,
+                onValueChange = {
+                    password = it
+                },
+                leadingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = null) },
+                trailingIcon = {
+                    IconButton(onClick = {
+                        passwordVisibility = !passwordVisibility
+                    }) {
+                        Icon(
+                            painter = passwordVisibilityIcon,
+                            contentDescription = null)
+                    }
+                },
+                visualTransformation = if(passwordVisibility)
+                    VisualTransformation.None else PasswordVisualTransformation(),
+                singleLine = true
+            )
+
+            Spacer(Modifier.height(20.dp))
+
+            FilledIconButton(
+                onClick = {
+                    // todo
+                },
+                shape = RoundedCornerShape(10),
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .fillMaxWidth()
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                    contentDescription = "login button",
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .border(
+                            width = 1.dp,
+                            color = LocalContentColor.current,
+                            shape = CircleShape
+                        )
+                )
+            }
+
+            Spacer(Modifier.height(40.dp))
+
+            Text(
+                text = "Forget password?",
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .clickable {
+                        Toast
+                            .makeText(context, "Clicked", Toast.LENGTH_SHORT)
+                            .show()
+                    },
+                style = MaterialTheme.typography.bodySmall
+            )
+
+            Spacer(Modifier.height(10.dp))
+
+            Row(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = "Don't have account? ",
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Text(
+                    modifier = Modifier
+                        .clickable {
+                            Toast
+                                .makeText(context, "Clicked", Toast.LENGTH_SHORT)
+                                .show()
+                        },
+                    text = "Sign Up",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
+        }
     }
+
+
 
 }
 
