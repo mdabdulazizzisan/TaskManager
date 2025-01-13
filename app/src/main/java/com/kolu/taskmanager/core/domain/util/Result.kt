68399572856afedbbbfe5301ade557e7c54ffa97
mdabdulazizzisan <binabdulaziz4983@gmail.com>
2025-01-13 08:26:16 +0600

    create Result interface for error handling

---
+++ app/src/main/java/com/kolu/taskmanager/core/domain/util/Result.kt
@@ -0,0 +1,28 @@
+package com.kolu.taskmanager.core.domain.util
+
+
+sealed interface Result<out D, out E: Error> {
+    sealed class Success<out D>(val data: D): Result<D, Nothing>
+    sealed class Error<out E: com.kolu.taskmanager.core.domain.util.Error>(val error: E): Result<Nothing, E>
+}
+
+inline fun <T, E: Error> Result<T, E>.onSuccess(action: (T) -> Unit) : Result<T, E>{
+    return when(this){
+        is Result.Success -> {
+            action(data)
+            this}
+        else -> this
+    }
+}
+
+inline fun <T, E: Error> Result<T, E>.onError(action: (E) -> Unit): Result<T, E>{
+    return when(this){
+        is Result.Error -> {
+            action(error)
+            this
+        }
+        is Result.Success -> this
+        else -> {this}
+    }
+
+}
